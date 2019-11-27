3a68c3b8a5560ca5e5ce1d2679c6e3b40169df1f
Jaid <jaid.jsx@gmail.com>
2019-11-27 02:15:28 +0100

    Testing
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import path from "path"
+
 import {exec} from "@actions/exec"
 import getActionTag from "lib/getActionTag"
+import {getInput} from "@actions/core"
+import fsp from "@absolunet/fsp"
 
 async function main() {
   const tag = getActionTag()
@@ -8,7 +12,30 @@ async function main() {
     return
   }
   console.log(`Tag: ${tag}`)
-  await exec("echo", ["Hello3"])
+  const pkgFile = path.resolve("package.json")
+  const pkgFileExists = await fsp.pathExists(pkgFile)
+  if (!pkgFileExists) {
+    console.log("package.json does not exist, skipping")
+    return
+  }
+  const pkg = await fsp.readJson5(pkgFile)
+  const packageName = pkg?.name
+  if (!packageName) {
+    console.log("package.json[name] is not set, skipping")
+    return
+  }
+  if (pkg.private) {
+    console.log("package.json[private] is true, skipping")
+    return
+  }
+  const npmToken = getInput("npmToken")
+  if (npmToken) {
+    const npmrcFileName = getInput("npmrcFile", {required: true})
+    const registryHost = getInput("npmRegistry", {required: true})
+    console.log(`Writing and registry host and npm token to ${npmrcFileName}`)
+    await fsp.outputFile(npmrcFileName, `//${registryHost}/:_authToken=${npmToken}`)
+    await exec("npm", ["publish"])
+  }
 }
 
 main()
