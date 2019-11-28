3a1e0679f5e7a94a841e97779ac90df5c08023c7
Jaid <jaid.jsx@gmail.com>
2019-11-28 05:00:51 +0100

    Improved log messages
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/publish.js
+++ src/publish.js
@@ -1,6 +1,7 @@
 import {exec} from "@actions/exec"
 import {getInput} from "@actions/core"
 import fsp from "@absolunet/fsp"
+import chalk from "chalk"
 
 export default async function ({registry, publishDirectory, dry, npmrcFileName}) {
   const {id} = registry
@@ -11,12 +12,12 @@ export default async function ({registry, publishDirectory, dry, npmrcFileName})
   }
   console.log(`Token given! (Length: ${token.length})`)
   const host = getInput(`${id}Registry`, {required: true})
-  console.log(`Registry host: ${host}`)
-  console.log(`Writing and registry host and npm token to ${npmrcFileName}`)
+  console.log(`${chalk.yellow(npmrcFileName)} content: ${chalk.blueBright(`//${host}/:_authToken=TOKEN`)}`)
   await fsp.outputFile(npmrcFileName, `//${host}/:_authToken=${token}`)
   const args = ["publish", publishDirectory]
   if (dry) {
     args.push("--dry-run")
   }
   await exec("npm", args)
+  await fsp.unlink(npmrcFileName)
 }
